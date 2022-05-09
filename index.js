71d5e73fab4223a131b6a7478b4f13a88c5e874a
JOMA-OFFICE <jomadelema@hotmail.com>
2022-05-09 10:16:09 -0600

    se creo servicio, controlador y ruta para listar usuarios

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ const postsRoutes = require('./routes/posts.route');
 app.use('/posts', postsRoutes);
 
 
+const usuariosRoutes = require('./routes/users.route');
+app.use('/usuarios', usuariosRoutes);
+
+
 app.listen(3100, ()=> {
     console.log("Escuchando....");
 });
