c9538fdb2772794527b4f1b91456fcc7c7a388c7
JOMADELEMA <jomadelema@gmail.com>
2022-05-09 00:00:08 -0600

    se agrego la funcionalidad de agregar post (servicio, controlador y ruta)

--- routes/posts.route.js
+++ routes/posts.route.js
@@ -4,5 +4,6 @@ var express= require('express');
 var router = express.Router();
 
 router.get("/listar-posts", postsController.listarPosts);
+router.post("/agregar-post", postsController.agregarPost);
 
 module.exports = router;
--- services/posts.service.js
+++ services/posts.service.js
@@ -1,14 +1,33 @@
-const db = require('../config/db.config');
+const db = require("../config/db.config");
 
-exports.listarPosts = (Data, callback)=>{
-    db.query(`
+exports.listarPosts = (Data, callback) => {
+  db.query(
+    `
     SELECT * FROM myern.post
-    `, [], 
-    (error, results, fields)=>{
-        if(error) {
-            console.log(error.message);
-            return callback(error);
-        };
-        return callback(null, results);
-    });
+    `,
+    [],
+    (error, results, fields) => {
+      if (error) {
+        console.log(error.message);
+        return callback(error);
+      }
+      return callback(null, results);
+    }
+  );
+};
+
+exports.agregarPost = (data, callback) => {
+  db.query(
+    `INSERT INTO 
+    myern.post (id_post, texto, fecha_creacion, id_usuario)
+    VALUES (?, ?, ?, ?)
+    `,
+    [data.id_post, data.texto, data.fecha_creacion, data.id_usuario],
+    (error, results, fields) => {
+      if (error) {
+        return callback(error);
+      }
+      return callback(null, "Post agregado exitosamente");
+    }
+  );
 };
