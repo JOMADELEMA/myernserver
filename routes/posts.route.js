e0517f12c293713e5267b95125f7baebf0e41c97
JOMADELEMA <jomadelema@gmail.com>
2022-05-12 23:11:09 -0600

    se corrigieron algunos detalles

--- routes/posts.route.js
+++ routes/posts.route.js
@@ -5,7 +5,8 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/listar-posts", validateToken, postsController.listarPosts);
-router.post("/agregar-post", postsController.agregarPost);
+router.post("/agregar-post", validateToken, postsController.agregarPost);
 router.post("/listar-mis-posts", validateToken, postsController.listarMisPosts);
+router.post("/post", validateToken, postsController.detallePost);
 
 module.exports = router;
