d3b57b640c98fff0c0ecdc207b2877488469d4e6
JOMA-OFFICE <jomadelema@hotmail.com>
2022-05-18 16:13:11 -0600

    se hizo correcciones

--- routes/etiquetas.route.js
+++ routes/etiquetas.route.js
@@ -6,5 +6,6 @@ var router = express.Router();
 
 router.post("/listar-etiquetas-proyecto", validateToken, etiquetasController.listarEtiquetasProyecto);
 router.get("/listar-etiquetas", validateToken, etiquetasController.listarEtiquetas);
+router.post("/agregar-etiquetas-proyecto", validateToken, etiquetasController.agregarEtiquetasProyecto);
 
 module.exports = router;
